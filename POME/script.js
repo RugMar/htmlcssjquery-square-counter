56a2ec65ab420213ff12431daf23898a3d3f1ea2
RugMar <ruggero.marina@gmail.com>
2019-03-04 15:25:56 +0100

    first new git, fffeeeega

---
+++ POME/script.js
@@ -0,0 +1,44 @@
+
+function printFirst() {
+  $("span#num").text("0");
+};
+printFirst();
+
+
+function printNumber() {
+
+  var left = $("#left");
+  var center = $("#center");
+  var right = $("#right");
+
+
+  left.click(function () {
+    var span = $("#num")
+    var val = span.text()
+    span.text( val - 1)
+  })
+
+  right.click(function () {
+    var span = $("#num")
+    var val = span.text()
+    val = Number(val)
+    span.text( val + 1)
+  })
+
+
+
+}
+
+
+printNumber();
+
+function changeColor() {
+  var center = $("#center")
+  var square = $(".square")
+  square.click(function () {
+    center.toggleClass("red")
+  })
+
+}
+
+changeColor()
